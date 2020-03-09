69b6e1262eaec4cd9cdeeefe4dfcf79d0e964c9d
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-09 04:59:21 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,11 +1,10 @@
 def my_each (collection)
   i=0
   while i < collection.length
-    puts collection[i]
     yield (collection[i])
     i+=1
   end
   collection
 end
 
-my_each (collection){|num| puts num.to_s}  
+my_each (collection){|num| puts num.to_s}
